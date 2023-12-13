7e4fd6b8f917bf2b899d857fd24d100013ba2dc8
TLZEPHYRUSM16\c1211 <c121122689@gmail.com>
2023-12-13 08:44:09 +0800

    增加註解

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -22,7 +22,7 @@
         @foreach ($cars as $car)
             <tr>
                 <td>{{ $car['id'] }}</td>
-                <td>{{ $car->store->name }}</td>
+                <td>{{ $car->store->name }}</td> <!-- 這裡是新增的 -->
                 <td>{{ $car['model'] }}</td>
                 <td>{{ $car['riding_noise'] }}</td>
                 <td>{{ $car['idle_noise'] }}</td>
